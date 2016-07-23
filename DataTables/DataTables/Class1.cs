0f1f59e26212c9b46800defd06156752f9a4481f
AShep1 <adrian.sheppard@gmail.com>
2016-07-23 17:44:55 +1000

    added test

--- DataTables/DataTables/Class1.cs
+++ DataTables/DataTables/Class1.cs
@@ -4,9 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.Data;
+
 namespace DataTables
 {
     public class Class1
     {
+
+        public static DataTable test()
+        {
+            DataTable dt = new DataTable();
+            int smallchange = 0;
+            return dt;
+        }
     }
 }
